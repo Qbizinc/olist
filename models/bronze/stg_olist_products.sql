0ea27fe53b6f15579186fc97f20f521744d348fd
jeff.eck@qbizinc.com <jeff.eck@qbizinc.com>
2025-05-30 16:23:54 +0000

    commit

--- models/bronze/stg_olist_products.sql
+++ models/bronze/stg_olist_products.sql
@@ -5,7 +5,7 @@
 SELECT
     p.product_id,
     p.product_category_name,
-    COALESCE(t.product_category_name_english, p.product_category_name) AS product_category_name_english, -- Handle missing translations
+    COALESCE(t.c2, p.product_category_name) AS product_category_name_english, -- Handle missing translations
     p.product_name_lenght AS product_name_length, -- Typo in source data
     p.product_description_lenght AS product_description_length, -- Typo in source data
     p.product_photos_qty,
@@ -13,6 +13,6 @@ SELECT
     p.product_length_cm,
     p.product_height_cm,
     p.product_width_cm
-FROM {{ source('olist_raw_data', 'olist_products_dataset') }} p
+FROM {{ source('olist_raw_data', 'products') }} p
 LEFT JOIN {{ source('olist_raw_data', 'product_category_name_translation') }} t
-    ON p.product_category_name = t.product_category_name
+    ON p.product_category_name = t.c1
