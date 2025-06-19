b9a2c8f86e20a3dfa03ec51fbf48d04e8cb85542
Jeff Eck <jeff.eck@qbizinc.com>
2025-06-19 16:56:30 +0000

    Push from Omni models/silver/dim_sellers.sql

--- models/silver/dim_sellers.sql
+++ models/silver/dim_sellers.sql
@@ -1,11 +1,14 @@
-SELECT "SELLER_ID",
-    "SELLER_ZIP_CODE_PREFIX",
+SELECT "dbt_jeck_bronze__stg_olist_sellers"."SELLER_ID",
+    "dbt_jeck_bronze__stg_olist_sellers"."SELLER_ZIP_CODE_PREFIX",
     CASE
-        WHEN LENGTH(CAST("SELLER_ZIP_CODE_PREFIX" AS VARCHAR)) = 4 THEN CONCAT('0', COALESCE(CAST("SELLER_ZIP_CODE_PREFIX" AS VARCHAR), ''))
-        ELSE CAST("SELLER_ZIP_CODE_PREFIX" AS VARCHAR)
+        WHEN LENGTH(CAST("dbt_jeck_bronze__stg_olist_sellers"."SELLER_ZIP_CODE_PREFIX" AS VARCHAR)) = 4 THEN CONCAT('0', COALESCE(CAST("dbt_jeck_bronze__stg_olist_sellers"."SELLER_ZIP_CODE_PREFIX" AS VARCHAR), ''))
+        ELSE CAST("dbt_jeck_bronze__stg_olist_sellers"."SELLER_ZIP_CODE_PREFIX" AS VARCHAR)
     END AS "SELLER_ZIP_CODE_FIXED",
-    "SELLER_CITY",
-    "SELLER_STATE",
-    CONCAT(COALESCE(CONCAT(COALESCE("SELLER_CITY", ''), ', '), ''), COALESCE("SELLER_STATE", '')) AS "SELLER_CITY_STATE"
-FROM "DBT_JECK_BRONZE"."STG_OLIST_SELLERS" AS "dbt_jeck_bronze__stg_olist_sellers"
-GROUP BY 4, 6, 1, 5, 3, 2
+    "dbt_jeck_bronze__stg_olist_sellers"."SELLER_CITY",
+    "dbt_jeck_bronze__stg_olist_sellers"."SELLER_STATE",
+    CONCAT(COALESCE(CONCAT(COALESCE("dbt_jeck_bronze__stg_olist_sellers"."SELLER_CITY", ''), ', '), ''), COALESCE("dbt_jeck_bronze__stg_olist_sellers"."SELLER_STATE", '')) AS "SELLER_CITY_STATE",
+    "dbt_jeck_bronze__stg_olist_geolocation"."GEOLOCATION_LAT",
+    "dbt_jeck_bronze__stg_olist_geolocation"."GEOLOCATION_LNG"
+FROM {{ref('stg_olist_sellers')}} AS "dbt_jeck_bronze__stg_olist_sellers"
+    LEFT JOIN {{ref('stg_olist_geolocation')}} AS "dbt_jeck_bronze__stg_olist_geolocation" ON "dbt_jeck_bronze__stg_olist_sellers"."SELLER_ZIP_CODE_PREFIX" = "dbt_jeck_bronze__stg_olist_geolocation"."GEOLOCATION_ZIP_CODE_PREFIX"
+GROUP BY 7, 8, 4, 6, 1, 5, 3, 2
